890b80001487aa7ffd5029bb7b53c8dc51c9d399
Marcus Fritzmann <marcus.fritzmann@googlemail.com>
2023-10-02 21:23:45 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-// #define MIRYOKU_ALPHAS_QWERTZ
-// #define MIRYOKU_TAP_QWERTZ
-// #define MIRYOKU_EXTRA_QWERTZ
-// #define MIRYOKU_CLIPBOARD_WIN
+  
+#define MIRYOKU_ALPHAS_QWERTZ
+#define MIRYOKU_TAP_QWERTZ
+#define MIRYOKU_EXTRA_QWERTZ
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
