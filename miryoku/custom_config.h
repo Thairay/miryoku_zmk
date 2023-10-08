a06c365e8251d368d7c7a0c5dc93ff8adf26905f
Marcus Fritzmann <marcus.fritzmann@googlemail.com>
2023-10-08 21:39:54 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -34,19 +34,29 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp DE_A_UMLAUT   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp DE_O_UMLAUT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&kp DE_U_UMLAUT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
 / {
-    combos {
-        compatible = "zmk,combos";
-        combo_ae {
-            timeout-ms = <50>;
-            key-positions = <3 11>;
-            bindings = <&kp DE_A_UMLAUT>;
-        };
+     combos {
+          compatible = "zmk,combos";
+          combo_ae {
+               timeout-ms = <50>;
+               key-positions = <3 13>;
+               bindings = <&kp DE_A_UMLAUT>;
+          };
+          combo_oe {
+               timeout-ms = <50>;
+               key-positions = <9 3>;
+               bindings = <&kp DE_O_UMLAUT>;
+          };
+           combo_ue {
+               timeout-ms = <50>;
+               key-positions = <3 7>;
+               bindings = <&kp DE_U_UMLAUT>;
+          };
     };
 };
 #if defined (MIRYOKU_KEYBOARD_CORNE)
